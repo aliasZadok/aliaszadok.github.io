87f52e62db40bdf6eccb5707cea7d98f82ff0960
aliasZadok <lfstone3@gmail.com>
2019-06-05 14:14:39 +0530

    Revert "405 fix #2"
    This reverts commit 3d3e9d3934b5d1f33acee96989c0bb7ff390058c.

--- form/js/app.js
+++ form/js/app.js
@@ -256,7 +256,7 @@ $cvvInput.on('input', createListener(isValidCVV));
 //form submition
 $('form').submit((e)=> {
   
-  e.preventDefault(e);
+  window.location.reload(true);
 
   const empty_name_input = $.trim($name.val()) === "";
   const empty_email_input = $.trim($emailInput.val()) === "";
